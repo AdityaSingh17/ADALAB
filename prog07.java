6d71a36cd04fec56048822cfe70fe5ab1cc8223b
Aditya Singh <47419182+AdityaSingh17@users.noreply.github.com>
2019-05-25 23:26:32 +0530

    Update prog07.java

--- prog07.java
+++ prog07.java
@@ -1,101 +1,72 @@
 import java.util.*;
-public class prog7 {
-	public static int findmin()
-	{
-		int i,n,min=0;
-		int d[] = new int [20];
-		int s[] = new int [20];
-		Scanner in = new Scanner(System.in);
-		n = in.nextInt();
-		for(i=1;i<=n;i++)
-		{
-			if(s[i]==0)
-			{
-				min=i;
-				break;
-			}
-		}
-		for(i=1;i<=n;i++)
-		{
-			if(d[i]<d[min] && s[i]==0)
-				min=i;
-		}
-		return min;		
-		
-	}
-	public void dijkstra(int v,int w[][],int s[],int d[],int n)
-	{
-		int i,w1,u,k,j;
-		int p[]=new int[20];
-		for(i=1;i<=n;i++)
-		{
-			s[i]=0;
-			d[i]=999;
-			p[i]=0;
-		}
-		d[v]=0;
-		for(k=1;k<=n;k++)
-		{
-			u=findmin();
-			s[u]=1;
-			for(w1=1;w1<=n;w1++)
-			{
-				if(w[u][w1]!=999 && s[w1]==0)
-				{
-					if(d[w1] > (d[u] +w[u][w1]))
-					{
-						d[w1]=d[u]+w[u][w1];
-						p[w1]=u;
-					}
-				}
-			}
-		}
-		System.out.println("SHORTEST PATH COST");
-		for(i=1;i<=n;i++)
-		{
-			if(d[i] == 999)
-			{
-				System.out.println(" NO PATH FROM "+v+" TO "+i);
-			}
-			else
-				System.out.println(" PATH FROM "+v+" TO "+i+" COSTS "+d[i]);
-		}
-		System.out.println("SHORTEST GROUP OF PATHS ARE");
-		for(i=1;i<=n;i++)
-		{
-			if(i!=v && d[i]!= 999)
-			{
-				System.out.println(i);
-				j=p[i];
-				while(p[j]!=0)
-				{
-					System.out.println("<---"+j+" ");
-					j=p[j];
-				}
-				System.out.println("<---"+v);
-			}
-		}
-	}
-	public static void main(String[] args) {
-		int i,j,n,v;
-		int d[]=new int[20];
-		int s[]=new int[20];
-		int w[][]=new int[20][20];
-		prog7 d1= new prog7();
-		Scanner in = new Scanner(System.in);
-		System.out.println("ENTER THE NUMBER OF VERTICES ");
-		n=in.nextInt();
-		System.out.println("ENTER COST ");
-		for(i=1;i<=n;i++)
-		{
-			for(j=1;j<=n;j++)
-			{
-				w[i][j]=in.nextInt();
-			}
-		}
-		System.out.println("ENTER SOURCE");
-		v=in.nextInt();
-		d1.dijkstra(v, w, s, d, n);
-	}
-
+ 
+public class Hamiltonian{
+    static boolean branchNbound(int pos, int x, int g[][], int path[]){
+        if(g[ path[ pos - 1]][x] == 0) return false;
+        for(int i = 0; i < pos; i++){
+            if(path[i] == x) return false;
+        }
+        return true;
+    }
+ 
+    static boolean HamCycle(int pos, int g[][], int path[], int n){
+ 
+        if(pos == n){
+            if(g[path[pos - 1]][path[0]] == 1)
+                return true;
+            else
+                return false;
+        }
+        for(int x = 1; x < n; x++){
+            boolean nextnode = branchNbound(pos, x, g, path);//see if next node can be added
+            if(nextnode){
+                path[pos] = x;
+                if(HamCycle( pos + 1, g, path, n)){
+                    return true;
+                }else{
+                    path[pos] = -1;
+                }
+            }
+        }
+       
+        return false;
+    }
+ 
+    public static void main(String[] args) {
+        int n,source;
+        boolean valid;
+        char ans;
+        Scanner in = new Scanner(System.in);
+        do{
+            System.out.print("\nEnter the number of nodes:\t");
+            n = in.nextInt();
+            int g[][] = new int[n][n];
+            int path[] = new int[n];
+            System.out.print("\nEnter the Adjacency Matrix(0/1):\n\n");
+            for(int i = 0; i < n; i++){
+                for(int j = 0 ; j < n; j++){
+                    g[i][j] = in.nextInt();
+                }
+            }
+            for(int i = 0; i < n; i++){
+                source = i;
+                valid = HamCycle(source + 1,g,path,n);
+                if(valid){
+                    System.out.print("\nHamiltonian cylce for source node "+source+":\n\n");//replace 0 by i
+                    for (int j = 0; j < n; j++) {
+                        System.out.print(path[j] + " ");
+                    }
+                    System.out.print(path[0]);
+                }else{
+                    System.out.print("\nHamiltonian cylce for source node doesn't exist!");
+                }
+ 
+                System.out.print("\n");
+            }
+           
+            System.out.print("\nEnter another graph?(Y/n):\t");
+            ans = in.next().charAt(0);
+ 
+        }while(ans == 'Y' || ans == 'y');
+    }
 }
